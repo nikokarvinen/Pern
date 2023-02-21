eb78eb5250e481291aaf735cfa70fe4e0fed69ce
AB8939 <AB8939@student.jamk.fi>
2023-02-21 11:40:40 +0200

    modified index

--- server/index.js
+++ server/index.js
@@ -31,7 +31,7 @@ app.post('/todos', async (req, res) => {
 app.get('/todos', async (req, res) => {
   try {
     const allTodos = await pool.query('SELECT * FROM todo');
-    res.json(allTodos).rows;
+    res.json(allTodos.rows);
   } catch (err) {
     console.log(err.message);
   }
@@ -53,7 +53,34 @@ app.get('/todos/:id', async (req, res) => {
 
 //update a todo
 
+app.put('/todos/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { description } = req.body;
+    const updateTodo = await pool.query(
+      'UPDATE todo SET description = $1 WHERE todo_id = $2',
+      [description, id]
+    );
+
+    res.json('Todo updated');
+  } catch (err) {
+    console.log(err.message);
+  }
+});
+
 //delete a todo
+
+app.delete('/todos/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleteTodo = await pool.query('DELETE FROM todo WHERE todo_id = $1', [
+      id,
+    ]);
+    res.json('Todo was deleted');
+  } catch (err) {
+    console.log(err.message);
+  }
+});
 
 app.listen(5000, () => {
   console.log('Server listening on port 5000');
